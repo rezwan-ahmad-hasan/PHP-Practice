8163b4fec97dc2a0ea97595da34e64001434187e
rezwan-ahmad-hasan <rezwanahmad@staffasia.com>
2023-11-24 11:27:27 +0600

    change, checking functions, checking define or not

--- index.php
+++ index.php
@@ -44,4 +44,19 @@ echo gettype($salary) . '<br>';
 // Print the whole variable with type
 var_dump($name, $age, $isMale, $height, $salary);
 
+// Change the value of the variable
+$name = false;
+
+// Variable checking functions
+is_string($name); // false
+is_int($age); // true
+is_bool($isMale); // true
+is_double($height); // true
+
+
+// Check if variable is defined
+var_dump(isset($name));
+var_dump(isset($name2));
+echo '<br>';
+
 ?>
