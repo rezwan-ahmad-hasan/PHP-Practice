2dbcf12d4c15ca12947d2d517802007d6fc8cf44
rezwan-ahmad-hasan <rezwanahmad@staffasia.com>
2023-11-24 11:14:33 +0600

    Print variables

--- index.php
+++ index.php
@@ -1,16 +1,31 @@
 <?php
 
-$a = 97;
+// What is a variable
 
-// echo $a++ .'<br>';
-// echo ++$a .'<br>';
+// Variable types
+/*
+    String
+    Integer
+    Float
+    Boolean
+    Null
+    Array
+    Object
+    Resource
+*/
 
-// echo $a-- .'<br>';
-// echo --$a .'<br>';
+// Declare variables
+$name = "Zura";
+$age = 28;
+$isMale = true;
+$height = 1.85;
+$salary = null;
 
-$number = 125619874984134.56446;
-$english_format_number = number_format($number, 3, '.', ',');
-echo $english_format_number;
-
+// Print the variables. Explain what is concatenation
+echo $name . '<br>';
+echo $age . '<br>';
+echo $isMale . '<br>';
+echo $height . '<br>';
+echo $salary . '<br>';
 
 ?>
