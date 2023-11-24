c321b32255ba8003fecfe78878123abe660dd483
rezwan-ahmad-hasan <rezwanahmad@staffasia.com>
2023-11-24 11:22:45 +0600

    print whole variable

--- index.php
+++ index.php
@@ -21,11 +21,27 @@ $isMale = true;
 $height = 1.85;
 $salary = null;
 
-// Print the variables. Explain what is concatenation
+// Constants declaration
+define('PI', 3.14);
+echo PI.'<br>';
+var_dump(defined('PI')); // defined(PI2)
+echo '<br>';
+
+// Print the variables. 
 echo $name . '<br>';
 echo $age . '<br>';
 echo $isMale . '<br>';
 echo $height . '<br>';
 echo $salary . '<br>';
+
+// Print variables types
+echo gettype($name) . '<br>';
+echo gettype($age) . '<br>';
+echo gettype($isMale) . '<br>';
+echo gettype($height) . '<br>';
+echo gettype($salary) . '<br>';
+
+// Print the whole variable with type
+var_dump($name, $age, $isMale, $height, $salary);
 
 ?>
